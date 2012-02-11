fc4290fc357ab0035f6b2d377a24c74363eb81a5
lichenghao888 <lichenghao888@yahoo.com.tw>
2012-02-11 15:52:30 +0800

    add write close method

--- cdata.c
+++ cdata.c
@@ -15,26 +15,43 @@
 #include <asm/io.h>
 #include <asm/uaccess.h>
 
+
 static int cdata_open(struct inode *inode, struct file *filp)
 {
-	return 0;
+    return 0;
 }
 
-static struct file_operations cdata_fops = {	
-	open:		cdata_open,
+ssize_t cdata_write(struct file *filp, const char *buf, size_t size,
+loff_t *off)
+{
+    return 0;
+}
+
+int cdata_close(struct inode *inode, struct file *filp)
+{
+    return 0;
+}
+
+static struct file_operations cdata_fops = {
+    open: cdata_open,
+    release: cdata_close,
+    write: cdata_write,
 };
 
 int cdata_init_module(void)
 {
-  register_chrdev(121, "cdata", &cdata_fops);
-  
+    if (register_chrdev(121, "cdata", &cdata_fops) < 0) {
+        printk(KERN_INFO "CDATA: can't register driver\n");
+    return -1;
+     }
+    return 0;
 }
 
 void cdata_cleanup_module(void)
 {
-
-
+    unregister_chrdev(121, "cdata");
 }
+
 
 module_init(cdata_init_module);
 module_exit(cdata_cleanup_module);
