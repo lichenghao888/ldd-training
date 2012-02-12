d99b32e9eec979c346420ed010520d7776cb76ea
lichenghao888 <lichenghao888@yahoo.com.tw>
2012-02-12 11:37:16 +0800

    add a simple test application

---
+++ test-simple.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/mman.h>
+
+void main()
+{
+    int fd;
+    fd = open("/dev/cdata4", O_RDWR);
+    sleep(10);
+
+    close(fd);
+}
