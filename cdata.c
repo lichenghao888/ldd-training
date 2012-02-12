6ae111b52d805389c4e3f786b7bd90587dc81556
lichenghao888 <lichenghao888@yahoo.com.tw>
2012-02-12 10:26:59 +0800

    add THIS_MODULE for kernel 2.6 moudule count

--- cdata.c
+++ cdata.c
@@ -17,39 +17,53 @@
 
 static int cdata_open(struct inode *inode, struct file *filp)
 {
-printk(KERN_INFO "CDATA: in open\n");
-return 0;
+    printk(KERN_INFO "CDATA: in open\n");
+    return 0;
+}
+
+ssize_t cdata_read(struct file *filp, const char *buf, size_t size,
+loff_t *off)
+{
+    return 0;
 }
 
 ssize_t cdata_write(struct file *filp, const char *buf, size_t size,
 loff_t *off)
 {
-return 0;
+    return 0;
 }
 
 int cdata_close(struct inode *inode, struct file *filp)
 {
-return 0;
+    return 0;
+}
+
+int cdata_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg)
+{
+
 }
 
 static struct file_operations cdata_fops = {
-open: cdata_open,
-release: cdata_close,
-write: cdata_write,
+    owner: THIS_MODULE,
+    open: cdata_open,
+    release: cdata_close,
+    read: cdata_read,    
+    write: cdata_write,
+    ioctl: cdata_ioctl,
 };
 
 int cdata_init_module(void)
 {
-if (register_chrdev(121, "cdata", &cdata_fops) < 0) {
-printk(KERN_INFO "CDATA: can't register driver\n");
-return -1;
-}
-return 0;
+    if (register_chrdev(121, "cdata", &cdata_fops) < 0) {
+        printk(KERN_INFO "CDATA: can't register driver\n");
+    return -1;
+     }
+    return 0;
 }
 
 void cdata_cleanup_module(void)
 {
-unregister_chrdev(121, "cdata");
+    unregister_chrdev(121, "cdata");
 }
 
 module_init(cdata_init_module);
